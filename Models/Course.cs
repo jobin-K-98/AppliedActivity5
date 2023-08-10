112009eb97fcc123b935821d0c6f865a21fbe4c5
jobin-K-98 <122489680+jobin-K-98@users.noreply.github.com>
2023-08-10 14:40:38 -0400

    created models
    Created the models for the application. Models student and Course is created

---
+++ Models/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppliedActivity5.Models
+{
+    [Table("Courses")]
+    public class Course
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string CourseCode { get; set; }
+
+        [MaxLength(200)]
+        public string Professor { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppliedActivity5.Models
+{
+    public class Student
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(200)]
+        public string Name { get; set; }
+        [MaxLength(200)]
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        [ForeignKey(typeof(Course))]
+        public int CourseId { get; set; }
+    }
+}
+
